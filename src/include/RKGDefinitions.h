359e57c51df37426cbf50df4f5f998b2f06d238b
DraXula <lennartnagel@web.de>
2022-05-19 07:57:03 +0200

    rkg enum interpretations

--- src/include/RKGDefinitions.h
+++ src/include/RKGDefinitions.h
@@ -241,6 +241,19 @@ public:
     } tail;
 
 public:
+    static constexpr TrackID TRACK_ID_ORDERED_LIST[32] =
+    {
+        TrackID::LC, TrackID::MMM, TrackID::MG, TrackID::TF,
+        TrackID::MC, TrackID::CM, TrackID::DKS, TrackID::WGM,
+        TrackID::DC, TrackID::KC, TrackID::MT, TrackID::GV,
+        TrackID::DDR, TrackID::MH, TrackID::BC, TrackID::RR,
+
+        TrackID::rPB, TrackID::rYF, TrackID::rGV2, TrackID::rMR,
+        TrackID::rSL, TrackID::rSGB, TrackID::rDS, TrackID::rWS,
+        TrackID::rDH, TrackID::rBC3, TrackID::rDKJP, TrackID::rMC,
+        TrackID::rMC3, TrackID::rPG, TrackID::rDKM, TrackID::rBC
+    };
+
     inline static QString ghostTypeDesc(const uint8_t ghostType)
     {
         if (ghostType == 0x1) return "Players Best Time (PB)";
@@ -261,4 +274,169 @@ public:
 
         return "";
     }
+
+    inline static QString trackDesc(const TrackID trackID)
+    {
+        switch (trackID)
+        {
+            case TrackID::LC: return "Luigi Circuit";
+            case TrackID::MMM: return "Moo Moo Meadows";
+            case TrackID::MG: return "Mushroom Gorge";
+            case TrackID::TF: return "Toads Factory";
+
+            case TrackID::MC: return "Mario Circuit";
+            case TrackID::CM: return "Coconut Mall";
+            case TrackID::DKS: return "DK Summit";
+            case TrackID::WGM: return "Warios Gold Mine";
+
+            case TrackID::DC: return "Daisy Circuit";
+            case TrackID::KC: return "Koopa Cape";
+            case TrackID::MT: return "Maple Treeway";
+            case TrackID::GV: return "Grumble Volcano";
+
+            case TrackID::DDR: return "Dry Dry Ruins";
+            case TrackID::MH: return "Moonview Highway";
+            case TrackID::BC: return "Bowser Castle";
+            case TrackID::RR: return "Rainbow Road";
+
+            case TrackID::rPB: return "GCN Peach Beach";
+            case TrackID::rYF: return "DS Yoshi Falls";
+            case TrackID::rGV2: return "SNES Ghost Valley 2";
+            case TrackID::rMR: return "N64 Mario Raceway";
+
+            case TrackID::rSL: return "N64 Sherbet Land";
+            case TrackID::rSGB: return "GBA Shy Guy Beach";
+            case TrackID::rDS: return "DS Delfino Square";
+            case TrackID::rWS: return "GCN Waluigi Stadium";
+
+            case TrackID::rDH: return "DS Desert Hills";
+            case TrackID::rBC3: return "GBA Bowsers Castle 3";
+            case TrackID::rDKJP: return "N64 Jungle Parkway";
+            case TrackID::rMC: return "GCN Mario Circuit";
+
+            case TrackID::rMC3: return "SNES Mario Circuit 3";
+            case TrackID::rPG: return "DS Peach Gardens";
+            case TrackID::rDKM: return "GCN DK Mountain";
+            case TrackID::rBC: return "N64 Bowsers Castle";
+        }
+    }
+
+    inline static QString vehicleDesc(const VehicleID vehicleID)
+    {
+        switch (vehicleID)
+        {
+            case VehicleID::sdf_kart: return "Standard Kart S";
+            case VehicleID::mdf_kart: return "Standard Kart M";
+            case VehicleID::ldf_kart: return "Standard Kart L";
+            case VehicleID::sa_kart: return "Booster Seat";
+            case VehicleID::ma_kart: return "Classic Dragster";
+            case VehicleID::la_kart: return "Offroader";
+            case VehicleID::sb_kart: return "Mini Beast";
+            case VehicleID::mb_kart: return "Wild Wing";
+            case VehicleID::lb_kart: return "Flame Fyler";
+            case VehicleID::sc_kart: return "Cheep Charger";
+            case VehicleID::mc_kart: return "Super Blooper";
+            case VehicleID::lc_kart: return "Piranha Prowler";
+            case VehicleID::sd_kart: return "Tiny Titan";
+            case VehicleID::md_kart: return "Daytripper";
+            case VehicleID::ld_kart: return "Jetsetter";
+            case VehicleID::se_kart: return "Blue Falcon";
+            case VehicleID::me_kart: return "Sprinter";
+            case VehicleID::le_kart: return "Honeycoupe";
+            case VehicleID::sdf_bike: return "Standard Bike S";
+            case VehicleID::mdf_bike: return "Standard Bike M";
+            case VehicleID::ldf_bike: return "Standard Bike L";
+            case VehicleID::sa_bike: return "Bullet Bike";
+            case VehicleID::ma_bike: return "Mach Bike";
+            case VehicleID::la_bike: return "Flame Runner";
+            case VehicleID::sb_bike: return "Bit Bike";
+            case VehicleID::mb_bike: return "Sugarscoot";
+            case VehicleID::lb_bike: return "Wario Bike";
+            case VehicleID::sc_bike: return "Quacker";
+            case VehicleID::mc_bike: return "Zip Zip";
+            case VehicleID::lc_bike: return "Shooting Star";
+            case VehicleID::sd_bike: return "Magikruiser";
+            case VehicleID::md_bike: return "Sneakster";
+            case VehicleID::ld_bike: return "Spear";
+            case VehicleID::se_bike: return "Jet Bubble";
+            case VehicleID::me_bike: return "Dolphin Dasher";
+            case VehicleID::le_bike: return "Phantom";
+        }
+    }
+
+    inline static QString characterDesc(const CharacterID characterID)
+    {
+        switch (characterID)
+        {
+            case CharacterID::mr: return "Mario";
+            case CharacterID::bpc: return "Baby Peach";
+            case CharacterID::wl: return "Waluigi";
+            case CharacterID::kp: return "Bowser";
+            case CharacterID::bds: return "Baby Daisy";
+            case CharacterID::ka: return "Dry Bones";
+            case CharacterID::bmr: return "Baby Mario";
+            case CharacterID::lg: return "Luigi";
+            case CharacterID::ko: return "Toad";
+            case CharacterID::dk: return "Donkey Kong";
+            case CharacterID::ys: return "Yoshi";
+            case CharacterID::wr: return "Wario";
+            case CharacterID::blg: return "Baby Luigi";
+            case CharacterID::kk: return "Toadette";
+            case CharacterID::nk: return "Koopa Troopa";
+            case CharacterID::ds: return "Daisy";
+            case CharacterID::pc: return "Peach";
+            case CharacterID::ca: return "Birdo";
+            case CharacterID::dd: return "Diddy Kong";
+            case CharacterID::kt: return "King Boo";
+            case CharacterID::jr: return "Bowser Jr.";
+            case CharacterID::bk: return "Dry Bowser";
+            case CharacterID::fk: return "Funky Kong";
+            case CharacterID::rs: return "Rosalina";
+            case CharacterID::sa_mii_m: return "Small Mii Outfit A (Male)";
+            case CharacterID::sa_mii_f: return "Small Mii Outfit A (Female)";
+            case CharacterID::sb_mii_m: return "Small Mii Outfit B (Male)";
+            case CharacterID::sb_mii_f: return "Small Mii Outfit B (Female)";
+            case CharacterID::sc_mii_m: return "Small Mii Outfit C (Male)";
+            case CharacterID::sc_mii_f: return "Small Mii Outfit C (Female)";
+            case CharacterID::ma_mii_m: return "Medium Mii Outfit A (Male)";
+            case CharacterID::ma_mii_f: return "Medium Mii Outfit A (Female)";
+            case CharacterID::mb_mii_m: return "Medium Mii Outfit B (Male)";
+            case CharacterID::mb_mii_f: return "Medium Mii Outfit B (Female)";
+            case CharacterID::mc_mii_m: return "Medium Mii Outfit C (Male)";
+            case CharacterID::mc_mii_f: return "Medium Mii Outfit C (Female)";
+            case CharacterID::la_mii_m: return "Large Mii Outfit A (Male)";
+            case CharacterID::la_mii_f: return "Large Mii Outfit A (Female)";
+            case CharacterID::lb_mii_m: return "Large Mii Outfit B (Male)";
+            case CharacterID::lb_mii_f: return "Large Mii Outfit B (Female)";
+            case CharacterID::lc_mii_m: return "Large Mii Outfit C (Male)";
+            case CharacterID::lc_mii_f: return "Large Mii Outfit C (Female)";
+            case CharacterID::m_mii: return "Medium Mii";
+            case CharacterID::s_mii: return "Small Mii";
+            case CharacterID::l_mii: return "Large Mii";
+            case CharacterID::pc_menu: return "Peach Biker Outfit";
+            case CharacterID::ds_menu: return "Daisy Biker Outfit";
+            case CharacterID::rs_menu: return "Rosalina Biker Outfit";
+        }
+    }
+
+    inline static QString controllerDesc(const ControllerType controllerID)
+    {
+        switch (controllerID)
+        {
+            case ControllerType::Wheel: return "Wii Wheel";
+            case ControllerType::Nunchuck: return "Wii Remote and Nunchuck";
+            case ControllerType::Classic: return "Classic Controller";
+            case ControllerType::GCN: return "Gamecube Controller";
+        }
+    }
+
+    inline static QString driftDesc(const DriftType driftID)
+    {
+        switch (driftID)
+        {
+            case DriftType::Manual: return "Manual";
+            case DriftType::Automatic: return "Automatic";
+        }
+    }
+
 };
